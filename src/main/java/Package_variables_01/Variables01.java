34c5f7fb3d217fd53396d3704d727e7080e2cf30
Oriol <ocamisubira@gmail.com>
2020-09-01 12:59:01 +0200

    Update_practices_JavaEE_01

---
+++ src/main/java/Package_variables_01/Variables01.java
@@ -0,0 +1,14 @@
+
+package Package_variables_01;
+
+public class Variables01 
+{
+    
+    public static void main(String[] args) 
+    {
+        int variableInt = 10;
+        
+        System.out.println(variableInt);
+    }
+    
+}
